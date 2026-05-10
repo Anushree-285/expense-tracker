794e94be7bae58630720e9b34108688882db17fb
Anushree-285 <user@users-MacBook-Air.local>
2026-05-10 19:07:41 +0530

    feat: add category support for expenses

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 expenses = []
 
-def add_expense(name, amount):
+def add_expense(name, amount, category):
     expense = {
         "name": name,
-        "amount": amount
+        "amount": amount,
+        "category": category
     }
+
     expenses.append(expense)
     print(f"{name} added successfully!")
 
 def view_expenses():
     print("\nExpense List:")
-    
+
     if not expenses:
         print("No expenses found.")
         return
@@ -18,12 +20,17 @@ def view_expenses():
     total = 0
 
     for expense in expenses:
-        print(f"{expense['name']} - ₹{expense['amount']}")
+        print(
+            f"{expense['name']} | "
+            f"{expense['category']} | "
+            f"₹{expense['amount']}"
+        )
+
         total += expense['amount']
 
     print(f"\nTotal Expense: ₹{total}")
 
-add_expense("Groceries", 1500)
-add_expense("Internet Bill", 999)
+add_expense("Groceries", 1500, "Food")
+add_expense("Netflix", 499, "Entertainment")
 
 view_expenses()
